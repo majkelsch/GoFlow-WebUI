cd2e8dcae495bb84ec60dc37015ba357b5ae3d53
majkelsch <m.schenk2006@seznam.cz>
2025-05-18 14:00:11 +0200

    Update 1.0.0

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, send_from_directory, render_template
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
+
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/profile')
+def logout():
+    return render_template('index.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
